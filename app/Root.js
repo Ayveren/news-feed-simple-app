b0500ae39a81a61e5ee7ebf6e1fa89b3202aa121
ayveren <ayverend@gmail.com>
2018-06-08 23:27:15 +0300

    fix fonts load for android

--- app/Root.js
+++ app/Root.js
@@ -2,9 +2,30 @@ import React, {Component} from 'react'
 import {Provider} from 'react-redux'
 import store from './store/configureStore'
 import Routes from './Routes'
+import { Font, AppLoading } from 'expo';
 
 export default class Root extends Component {
+  state = {
+    isReady: false,
+  };
+
+  async cashFonts() {
+    await Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
+    });
+  }
+
   render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this.cashFonts}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
+    }
     return (
       <Provider store={store}>
         <Routes/>
