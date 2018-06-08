c0ef1855a4c4626ec9824c0ba5d14415ff5d6f1b
ayveren <ayverend@gmail.com>
2018-06-08 23:28:56 +0300

    add reducers for feed actions

--- app/reducers/feedReducer.js
+++ app/reducers/feedReducer.js
@@ -1,12 +1,11 @@
-import * as actions from '../actions/authActions';
+import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 
 export default function feedReducer(state = initialState.articles, action) {
   switch (action.type) {
-    case actions.GET_NEWS_SUCCESS:
-      console.log(action);
-      return state.articles;
-    case actions.GET_NEWS_ERROR:
+    case types.GET_NEWS_SUCCESS:
+      return action.data.articles;
+    case types.GET_NEWS_ERROR:
     default:
       return state;
   }
