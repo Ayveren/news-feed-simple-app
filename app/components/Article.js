4a0a3288c642f6b85301e4dee9adbd39d24d91d2
ayveren <ayverend@gmail.com>
2018-06-18 15:52:34 +0300

    add external links

--- app/components/Article.js
+++ app/components/Article.js
@@ -1,38 +1,40 @@
 import React, {Component} from 'react';
-import {Image} from 'react-native';
+import {Image, TouchableOpacity, Linking} from 'react-native';
 import {Card, CardItem, Text} from 'native-base';
 import {dimension} from '../styles';
 
 export default class Article extends Component {
   render() {
-    const {title, description, urlToImage} = this.props.data;
+    const {title, description, urlToImage, url} = this.props.data;
 
     return (
       <Card>
+        <TouchableOpacity onPress={() => {
+          Linking.openURL(url);
+        }}>
+          <CardItem>
+            <Text>
+              {title}
+            </Text>
+          </CardItem>
 
-        <CardItem>
-          <Text>
-            {title}
-          </Text>
-        </CardItem>
+          <CardItem cardBody>
+            <Image
+              style={{
+                resizeMode: 'cover',
+                width: dimension().width,
+                height: 150
+              }}
+              source={{uri: urlToImage}}
+            />
+          </CardItem>
 
-        <CardItem cardBody>
-          <Image
-            style={{
-              resizeMode: 'cover',
-              width: dimension().width,
-              height: 150
-            }}
-            source={{uri: urlToImage}}
-          />
-        </CardItem>
-
-        <CardItem>
-          <Text note>
-            {description}
-          </Text>
-        </CardItem>
-
+          <CardItem>
+            <Text note>
+              {description}
+            </Text>
+          </CardItem>
+        </TouchableOpacity>
       </Card>
     )
   }
